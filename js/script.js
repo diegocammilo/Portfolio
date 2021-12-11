5f089c7e51d10e21ce9384cc891cc2ad7fae84e4
diegocammilo <diegocamilo_@hotmail.com>
2021-12-11 10:04:51 -0300

    :memo:

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+window.addEventListener('scroll', ()=>{
+    const pagetop=window.pageYOffset<=0;
+    const containerscroll=document.querySelector('.containerscroll');
+    containerscroll.style.display=pagetop?'none':'block';
+},false)
